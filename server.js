f70408a8934d5e38d21380acde9bf5ba8ce33f5a
zhenglinj <zhenglinj@hotmail.com>
2017-06-06 19:09:55 +0800

    Update server.js to compile jsx

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config');
 
 var app = express();
+var compiler = webpack(config);
+
+compiler.run((err, stats) => {
+  if (err) {
+    console.log(err);
+    console.log(stats);
+    return;
+  }
+});
 
 app.set('port', (process.env.PORT || 5000));
 
